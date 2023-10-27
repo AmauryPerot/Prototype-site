4b1d7c6bfe92aea89deb7710a51e2a9f26b580b7
AmauryPerot <149076640+AmauryPerot@users.noreply.github.com>
2023-10-27 16:32:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Change la couleur de fond*/
+function couleur(couleur){
+  document.body.style.backgroundColor = couleur;
+}
+
+/* Change la couleur du fond du texte*/
+function fondcouleur(id,couleur){ document.getElementById(id).style.backgroundColor=couleur;
+}
